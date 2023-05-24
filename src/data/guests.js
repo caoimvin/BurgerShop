f5acca118277a4b79e6d90eceb9696376e6ee9fc
kevinmissback <kevin.missback@googlemail.com>
2023-05-25 00:55:03 +0200

    guests data with random colors and speed

--- src/data/guests.js
+++ src/data/guests.js
@@ -1,7 +1,31 @@
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getSkinColor() {
+  const colors = ['#8d5524', '#c68642', '#e0ac69', '#f1c27d', '#ffdbac']
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function createGuestColors() {
+  return [getSkinColor(), getRandomColor(), getRandomColor()]
+}
+
+function getSpeed() {
+  const max = 10
+  const min = 5
+  return (Math.random() * (max - min + 1) + min).toFixed(1)
+}
+
 const guests = [
     {
       id: 1,
-      color: 'blue',
+      // color: getRandomColor(),
       status: 'entering',
       delay: 0,
       happiness: 100,
@@ -13,7 +37,7 @@ const guests = [
       }
     },{
       id: 2,
-      color: 'green',
+      // color: 'green',
       status: 'entering',
       delay: 2,
       happiness: 80,
@@ -21,11 +45,11 @@ const guests = [
         bun: 'default',
         meat: 'chicken',
         toppings: ['salad'],
-        extras: []
+        extras: ['coke', 'sweet']
       }
     },{
       id: 3,
-      color: 'red',
+      // color: 'red',
       status: 'entering',
       delay: 5,
       happiness: 95,
@@ -38,4 +62,8 @@ const guests = [
     }
   ]
 
-  export default guests
+  const guestsFormatted = guests.map(guest => ({...guest, color: createGuestColors(), speed: getSpeed()}))
+
+  // console.log(guestsFormatted);
+
+  export default guestsFormatted
