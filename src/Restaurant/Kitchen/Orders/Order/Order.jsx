cbf470f6c1b2accb330f5e8bc42aa133db58418c
kevinmissback <kevin.missback@googlemail.com>
2023-05-25 02:37:56 +0200

    restructure order content positions

--- src/Restaurant/Kitchen/Orders/Order/Order.jsx
+++ src/Restaurant/Kitchen/Orders/Order/Order.jsx
@@ -4,20 +4,20 @@ import styles from './Order.module.css'
 function Order({ guest, serveOrder, index, next, prev, max }) {
     return (
       <div className={styles.order}>
-        <div className={styles.guest_image}>
-          {/* {guest.color} */}
-            <div style={{ background: guest.color[0] }}></div>
-            <div style={{ background: guest.color[1] }}></div>
-            <div style={{ background: guest.color[2] }}></div>
-        </div>
-          <div className={styles.guest_happiness}>
-            {/* {guest.happiness} */}
-            <div style={{ width: `${100 - guest.happiness}%` }}></div>
+        <div className={styles.label}>
+          <div className={styles.guest_image}>
+              <div style={{ background: guest.color[0] }}></div>
+              <div style={{ background: guest.color[1] }}></div>
+              <div style={{ background: guest.color[2] }}></div>
           </div>
-        <div className={`${styles.label} ${styles.guest_number}`}>
-          number {index + 1} of {max} items
-          <button onClick={() => prev()}>prev</button>
-          <button onClick={() => next()}>next</button>
+          <div className={styles.guest_number}>
+            {max > 1 && <button onClick={() => prev()}>&#65124;</button>}
+            <p>Guest {index + 1} of {max}</p>
+            {max > 1 && <button onClick={() => next()}>&#65125;</button>}
+          </div>
+        </div>
+        <div className={styles.guest_happiness}>
+          <div style={{ width: `${100 - guest.happiness}%` }}></div>
         </div>
         <div className={styles.guest_order}>
           <OrderPreview order={guest.order} />
